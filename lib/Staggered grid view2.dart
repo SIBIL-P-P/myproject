7a3a2fac057dd6e64827b4b5afd757e70e28bacf
SIBIL <sibilshivadasan1234@gmail.com>
2023-01-10 20:58:03 +0530

    commit

--- lib/Staggered grid view2.dart
+++ lib/Staggered grid view2.dart
@@ -1,107 +1,107 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
-
-import 'Staggered grid view.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: StaggeredGridView2(),
-  ));
-}
-
-class StaggeredGridView2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-          child: StaggeredGridView.count(
-            crossAxisCount: 4,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            children: [
-              StaggeredGridTile.count(
-                crossAxisCellCount: 2,
-                mainAxisCellCount: 2,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 2,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 1,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 1,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 4,
-                mainAxisCellCount: 2,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 2,
-                mainAxisCellCount: 2,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 2,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 1,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 1,
-                mainAxisCellCount: 1,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-              StaggeredGridTile.count(
-                crossAxisCellCount: 4,
-                mainAxisCellCount: 2,
-                child: Customchild(
-                  iconData: Icons.add,
-                  bgcolor: Colors.red,
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+//
+// import 'Staggered grid view.dart';
+//
+// void main() {
+//   runApp(MaterialApp(
+//     home: StaggeredGridView2(),
+//   ));
+// }
+//
+// class StaggeredGridView2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SingleChildScrollView(
+//           child: StaggeredGridView.count(
+//             crossAxisCount: 4,
+//             mainAxisSpacing: 4,
+//             crossAxisSpacing: 4,
+//             children: [
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 2,
+//                 mainAxisCellCount: 2,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 2,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 1,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 1,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 4,
+//                 mainAxisCellCount: 2,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 2,
+//                 mainAxisCellCount: 2,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 2,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 1,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 1,
+//                 mainAxisCellCount: 1,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//               StaggeredGridTile.count(
+//                 crossAxisCellCount: 4,
+//                 mainAxisCellCount: 2,
+//                 child: Customchild(
+//                   iconData: Icons.add,
+//                   bgcolor: Colors.red,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ));
+//   }
+// }
--- lib/navigation drawer.dart
+++ lib/navigation drawer.dart
@@ -1,15 +1,98 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class navDrawer extends StatelessWidget{
+void main() {
+  runApp(MaterialApp(
+    home: drawer(),
+    debugShowCheckedModeBanner: false
+  ));
+}
+
+class drawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Drawer"),
-        
-      ),
-    );
+        appBar: AppBar(),
+        endDrawer: Drawer(
+            child: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.orange,Colors.pinkAccent
+                        ])),
+                child: ListView(children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage("https://c8.alamy.com/comp/2G7W1A0/dl-logo-initial-letter-monogram-with-abstrac-circle-shape-design-template-isolated-in-white-background-2G7W1A0.jpg"),
+                      ),
+                      title: Text("SIBIL",style: TextStyle(color: Colors.white38),),
+                      subtitle: Text("sibilshivadasan12334@gmail.com",style: TextStyle(color: Colors.white38),),
+                      trailing: Icon(Icons.close,color: Colors.white38),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: ListTile(
+                      leading: Icon(Icons.dashboard,color: Colors.white38),
+                      title: Text("Dashboard",style: TextStyle(color: Colors.white38),),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.leaderboard_sharp,color: Colors.white38),
+                    title: Text("Leads",style: TextStyle(color: Colors.white38),),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.people,color: Colors.white38,
+                    ),
+                    title: Text(
+                      "clients",style: TextStyle(color: Colors.white38),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.rocket,color: Colors.white38,
+                    ),
+                    title: Text(
+                      "Projects",style: TextStyle(color: Colors.white38),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.handshake_sharp,color: Colors.white38),
+                    title: Text("partners",style: TextStyle(color: Colors.white38),),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.subscriptions,color: Colors.white38),
+                    title: Text("Subscription",style: TextStyle(color: Colors.white38),),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.money,color: Colors.white38),
+                    title: Text("Payment",style: TextStyle(color: Colors.white38),),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.verified_user,color: Colors.white38),
+                    title: Text("Users",style: TextStyle(color: Colors.white38),),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.library_add_check,color: Colors.white38,),
+                    title: Text("library",style: TextStyle(color: Colors.white38),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100,left: 10,right: 10),
+                    child: Container(
+                      height: 30,
+                      child: ElevatedButton(style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
+                          backgroundColor: Colors.orange.shade300
+                      ),
+                          onPressed: () {}, child: Text("Logout")),
+                    ),
+                  )
+                ]))));
   }
-
 }
